81385fc68c337dcb0b835d8d2b779991740ff4d0
christian <sosa.christian.agustin@gimail.com>
2019-11-23 13:16:15 -0300

    prueba

--- resources/views/agregar.blade.php
+++ resources/views/agregar.blade.php
@@ -3,20 +3,27 @@
 @section('contenido')
 <main>
   <form class="" action="/agregar" method="post" enctype="multipart/form-data">
-   {{csrf_field()}}
+    {{csrf_field()}}
     <div class="titulo row">
       <h2>Agregar producto</h2>
     </div>
     <div class="form-group row">
       <label for="nombre" class="col-sm-4 col-form-label">Nombre</label>
       <div class="col-sm-6">
+
         <input type="text" class="form-control" id="nombre" name="nombre" placeholder="" value="{{old('nombre')}}">
+        @error('nombre')
+        {{$message}}
+        @enderror
       </div>
     </div>
     <div class="form-group row">
       <label class="col-sm-4 col-form-label" for="precio">Precio</label>
       <div class="col-sm-6">
         <input type="text" class="form-control" id="precio" name="precio" value="{{old('precio')}}">
+        @error('precio')
+        {{$message}}
+        @enderror
       </div>
 
     </div>
@@ -24,6 +31,9 @@
       <label class="col-sm-4 col-form-label" for="descripcion">Descripcion</label>
       <div class="col-sm-6">
         <textarea class="form-control" name="descripcion" id="descripcion" rows="2" cols="20">{{old('descripcion')}}</textarea>
+        @error('descripcion')
+        {{$message}}
+        @enderror
       </div>
 
     </div>
@@ -32,8 +42,11 @@
       <div class="col-sm-6">
         <select class="form-control" name="categoria_id" id="categoria">
           @foreach($categorias as $categoria)
-            <option value="{{$categoria['id']}}">{{$categoria->nombre}}</option>
+          <option value="{{$categoria['id']}}">{{$categoria->nombre}}</option>
           @endforeach
+          @error('categoria')
+          {{$message}}
+          @enderror
         </select>
       </div>
     </div>
@@ -41,16 +54,10 @@
       <label class="col-sm-4 col-form-label" for="imagen">Imagen</label>
       <div class="col-sm-6">
         <input type="file" class="form-control" id="imagen" name="imagen" value="">
+        @error('imagen')
+        {{$message}}
+        @enderror
       </div>
-    </div>
-    <div class="errors">
-      <ul>
-        @foreach ($errors->all() as $error)
-          <li>
-            {{$error}}
-          </li>
-        @endforeach
-      </ul>
     </div>
     <div class="form-group row">
       <div class="col-8"></div>
